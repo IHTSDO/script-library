e0a4ce799dc3de2ffb22c8eaa8d016fd1a819f1f
pgwilliams <petergwilliams@hotmail.com>
2026-03-20 17:23:32 +0000

    QI-1322 Normalise FloroAngiography

--- src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
+++ src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
@@ -442,9 +442,10 @@ public class NormaliseTemplateCompliantConcepts extends TemplateFix {
 		templateNames = new String[] { "templates/procedures/Construction of stoma.json" };
 		templateNames = new String[] { "templates/procedures/Radiotherapy.json" };
 		templateNames = new String[] { "templates/procedures/Drainage.json" };
+		templateNames = new String[] { "templates/procedures/Decompression.json" };
 		*/
 
-		templateNames = new String[] { "templates/procedures/Decompression.json" };
+		templateNames = new String[] { "templates/procedures/FluroAngiographyAndStent.json" };
 		
 		//TODO We're seeing 'HIGH' warnings about existing parents being redundant in presence of PPP but before the PPP gets added. Investigate
 		//I think this might happen when we set a PPP which is lower than the existing parent.
