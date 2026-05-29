efc4a2420d9544a3730d9fa18af2be9cdc63ce26
pgwilliams <petergwilliams@hotmail.com>
2026-05-29 16:12:49 +0100

    INFRA-17243 Add support for concept promotion selected via task review entry

---
+++ src/main/java/org/ihtsdo/termserver/scripting/delta/ExtractExtensionComponentSelectedFromReview.java
@@ -0,0 +1,19 @@
+package org.ihtsdo.termserver.scripting.delta;
+
+import java.util.List;
+
+public class ExtractExtensionComponentSelectedFromReview extends ExtractExtensionComponents {
+	private static List<String> tasksWithReviews = List.of(
+			"HQ-21",
+			"HQ-28",
+			"HQ-29",
+			"HQ-32",
+			"HQ-33",
+			"HQ-38",
+			"HQ-46",
+			"HQ-30",
+			"HQ-36",
+			"HQ-34",
+			"HQ-55"
+	);
+}
