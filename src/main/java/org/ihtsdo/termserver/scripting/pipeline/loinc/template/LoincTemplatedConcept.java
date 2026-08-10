05ed24f93f63ebf56855d42cebead9ec82786a14
pgwilliams <petergwilliams@hotmail.com>
2026-08-10 10:51:44 +0200

    LE-174 Modify termining rules for {Specimen} post coordinated LOINC concepts

--- src/main/java/org/ihtsdo/termserver/scripting/pipeline/loinc/template/LoincTemplatedConcept.java
+++ src/main/java/org/ihtsdo/termserver/scripting/pipeline/loinc/template/LoincTemplatedConcept.java
@@ -66,7 +66,12 @@ public abstract class LoincTemplatedConcept extends TemplatedConcept implements 
 	protected static Set<String> useTypesInPrimitive = new HashSet<>(Arrays.asList("SYSTEM", "METHOD", "SCALE", "TIME"));
 	protected static Set<String> skipLDTColumnNames = new HashSet<>(List.of("SYSTEMCORE_PN","TIMECORE_PN"));
 	protected static Set<String> columnsToCheckForUnknownIndicators = new HashSet<>(Arrays.asList(COMPNUM_PN, COMPDENOM_PN, SYSTEM_PN));
-	protected static Set<String> allowSpecimenTermForLoincParts = new HashSet<>(Arrays.asList("LP7593-9", "LP7735-6", "LP189538-4"));
+
+	protected static Set<String> allowSpecimenTermForLoincParts = new HashSet<>(Arrays.asList(
+			"LP7593-9",
+			"LP7735-6",
+			"LP189538-4",
+			"LP451871-0"));
 
 	protected static Set<String> unknownIndicators = new HashSet<>(Arrays.asList(
 			"unidentified", "other",
